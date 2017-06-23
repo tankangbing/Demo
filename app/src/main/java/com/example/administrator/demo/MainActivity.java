a5b42f83ba2afcccfb11510c6e7be36265251d68
tan <1152708287@qq.com>
2017-06-23 11:58:13 +0800

    webView的集成

--- app/src/main/java/com/example/administrator/demo/MainActivity.java
+++ app/src/main/java/com/example/administrator/demo/MainActivity.java
@@ -1,23 +1,104 @@
 package com.example.administrator.demo;
 
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    LinearLayout btn1 ,btn2,btn3, btn4,btn5, btn6,btn7, btn8;
+    //    LinearLayout btn1;
+    private long exitTime = 0;
+    //    Toast tst;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         initView();
         initData();
+        initEvent();
+    }
+    private void initEvent() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            SystemBarTintManager tintManager = new SystemBarTintManager(this);
+            tintManager.setStatusBarTintEnabled(true);
+            tintManager.setStatusBarTintResource(R.color.colorPrimaryDark);
+        }
+    }
+    private void initData() {
+        //hehe
     }
 
     private void initView() {
-        int i = 0;
+        setContentView(R.layout.activity_main);
+        btn1 = (LinearLayout) findViewById(R.id.btn_mreasure);
+        btn2 = (LinearLayout) findViewById(R.id.btn_remind);
+        btn3 = (LinearLayout) findViewById(R.id.btn_curve);
+        btn4 = (LinearLayout) findViewById(R.id.btn_show);
+        btn5 = (LinearLayout) findViewById(R.id.btn_set);
+        btn6 = (LinearLayout) findViewById(R.id.btn_ii);
+        btn7 = (LinearLayout) findViewById(R.id.btn_powerkm);
+        btn8 = (LinearLayout) findViewById(R.id.btn_guide);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+        btn8.setOnClickListener(this);
     }
 
-    private void initData() {
-        int j = 1;
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_mreasure:
+//                Toast.makeText(getApplicationContext(), "Hello,I come from other thread!",Toast.LENGTH_SHORT ).show();
+                //测量血压
+                Intent intent1=new Intent(MainActivity.this,WebActivity.class);
+                startActivity(intent1);
+                break;
+            case R.id.btn_remind:
+
+                break;
+            case R.id.btn_curve:
+
+                break;
+            case R.id.btn_show:
+
+                break;
+            case R.id.btn_set:
+                break;
+            case R.id.btn_ii:
+                break;
+            case R.id.btn_powerkm:
+
+                break;
+            case R.id.btn_guide:
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    //返回键添加提醒
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN){
+            if((System.currentTimeMillis()-exitTime) > 2000){
+                Toast.makeText(getApplicationContext(), "再按一次退出程序", Toast.LENGTH_SHORT).show();
+                exitTime = System.currentTimeMillis();
+            } else {
+                finish();
+                System.exit(0);
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
---
+++ app/src/main/java/com/example/administrator/demo/WebActivity.java
@@ -0,0 +1,99 @@
+package com.example.administrator.demo;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static com.example.administrator.demo.R.id.webView;
+
+public class WebActivity extends AppCompatActivity{
+
+    private WebView mWebView;
+    private WebSettings mWebSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initView();
+        initToobar();
+        initData();
+    }
+
+    private void initToobar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            SystemBarTintManager tintManager = new SystemBarTintManager(this);
+            tintManager.setStatusBarTintEnabled(true);
+            tintManager.setStatusBarTintResource(R.color.colorPrimaryDark);
+        }
+    }
+
+    private void initView() {
+        setContentView(R.layout.activity_web);
+        mWebView = (WebView)findViewById(webView);
+
+    }
+
+    private void initData() {
+        mWebSettings = mWebView.getSettings();
+
+//如果访问的页面中要与Javascript交互，则webview必须设置支持Javascript
+        mWebSettings.setJavaScriptEnabled(true);
+
+
+//设置自适应屏幕，两者合用
+        mWebSettings.setUseWideViewPort(true); //将图片调整到适合webview的大小
+        mWebSettings.setLoadWithOverviewMode(true); // 缩放至屏幕的大小
+
+//缩放操作
+        mWebSettings.setSupportZoom(true); //支持缩放，默认为true。是下面那个的前提。
+        mWebSettings.setBuiltInZoomControls(true); //设置内置的缩放控件。若为false，则该WebView不可缩放
+        mWebSettings.setDisplayZoomControls(false); //隐藏原生的缩放控件
+
+//其他细节操作
+        mWebSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK); //关闭webview中缓存
+        mWebSettings.setAllowFileAccess(true); //设置可以访问文件
+        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(true); //支持通过JS打开新窗口
+        mWebSettings.setLoadsImagesAutomatically(true); //支持自动加载图片
+        mWebSettings.setDefaultTextEncodingName("utf-8");//设置编码格式
+        String s = "http://192.168.1.58:8088/smLibrary";
+        mWebView.loadUrl(s);
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+    }
+    @Override
+    //覆盖Activity类的onKeyDown(int keyCoder,KeyEvent event)方法
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+            mWebView.goBack(); //goBack()表示返回WebView的上一页面
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mWebView != null) {
+            mWebView.loadDataWithBaseURL(null, "", "text/html", "utf-8", null);
+            mWebView.clearHistory();
+
+            ((ViewGroup) mWebView.getParent()).removeView(mWebView);
+            mWebView.destroy();
+            mWebView = null;
+        }
+        super.onDestroy();
+    }
+}
